237659fd0280469b26f1ae8b3a39b2d833d9a9f4
Antonio Ávila <koavdev@gmail.com>
2024-10-03 22:26:20 -0300

    feat: add X logo component

---
+++ components/Logo/X.vue
@@ -0,0 +1,12 @@
+<template>
+    <img src="@/assets/images/x_icon2.png" alt="Ícone X" :style="darkMode ? {filter: 'brightness(0) invert(1)'} : {}"/>
+</template>
+
+<script setup>
+const props = defineProps({
+    darkMode: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
