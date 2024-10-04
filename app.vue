a6e441c2f05a1f8db49a5ade38acb46404c0e1c4
Antonio Ávila <koavdev@gmail.com>
2024-10-03 22:26:42 -0300

    feat: add LeftSidebar component

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="dark">
-    <h1 class="dark:text-dim-500">Twitter clone</h1>
+  <div :class="{'dark': isDarkMode}">
+  <div class="bg-white dark:bg-dim-900">
+    <div class="min-h-full">
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+        <!-- Left sidebar -->
+        
+        <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+          <div class="sticky top-0">
+            <SidebarLeft />
+          </div>
+        </div>
+        <!-- Main content -->
+    
+        <!-- Right sidebar -->
+      </div>
+    
+    </div>
   </div>
+</div>
 </template>
+
+<script setup>
+import { useDarkMode } from '@/composables/useDarkMode';
+const { isDarkMode, toggleDarkMode } = useDarkMode();
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="h-screen flex flex-col">
+        <div
+            class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20"
+            :class="defaultTransition">
+            <nuxt-link to="/">
+                <div class="w-8 h-8">
+                    <LogoX :darkMode="isDarkMode" @click="toggleDarkMode"/>
+                </div>
+            </nuxt-link>
+        </div>
+
+        <div class="mt-2 space-y-3">    
+            <SidebarLeftTab active>
+                <template #icon>
+                    <HomeIcon />
+                </template>
+                <template #name>
+                    Home
+                </template>
+            </SidebarLeftTab>
+    
+            <SidebarLeftTab>
+                <template #icon>
+                    <HashtagIcon />
+                </template>
+                <template #name>
+                    Explore
+                </template>
+            </SidebarLeftTab>
+
+            <SidebarLeftTab>
+                <template #icon>
+                    <BellIcon />
+                </template>
+                <template #name>
+                    Notifications
+                </template>
+            </SidebarLeftTab>
+    
+            <SidebarLeftTab>
+                <template #icon>
+                    <InboxIcon />
+                </template>
+                <template #name>
+                    Messages
+                </template>
+            </SidebarLeftTab>
+            
+            <SidebarLeftTab>
+                <template #icon>
+                    <BookmarkIcon />
+                </template>
+                <template #name>
+                    Bookmarks
+                </template>
+            </SidebarLeftTab>
+
+            <SidebarLeftTab>
+                <template #icon>
+                    <DocumentTextIcon />
+                </template>
+                <template #name>
+                    Lists
+                </template>
+            </SidebarLeftTab>
+
+            <SidebarLeftTab>
+                <template #icon>
+                    <UserIcon />
+                </template>
+                <template #name>
+                    Profile
+                </template>
+            </SidebarLeftTab>
+
+            <SidebarLeftTab>
+                <template #icon>
+                    <EllipsisHorizontalCircleIcon/>
+                </template>
+                <template #name>
+                    More
+                </template>
+            </SidebarLeftTab>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { HomeIcon, HashtagIcon, BellIcon, InboxIcon, BookmarkIcon, DocumentTextIcon, UserIcon, EllipsisHorizontalCircleIcon  } from '@heroicons/vue/24/outline';
+import { useDarkMode } from '@/composables/useDarkMode';
+
+const { defaultTransition } = useTailwindConfig();
+
+const { isDarkMode, toggleDarkMode } = useDarkMode();
+</script>
