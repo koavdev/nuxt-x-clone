d4396cae42d84e8202cbab649ad0ffaf901feeba
Antonio Ávila <koavdev@gmail.com>
2024-10-22 22:12:57 -0300

    feat: add RightSidebar component

--- app.vue
+++ app.vue
@@ -10,9 +10,19 @@
             <SidebarLeft />
           </div>
         </div>
+
+
         <!-- Main content -->
+         <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
+          <h1>ASD</h1>
+         </main>
     
         <!-- Right sidebar -->
+        <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+          <div class="sticky top-0">
+            <SidebarRight />
+          </div>
+        </div>
       </div>
     
     </div>
